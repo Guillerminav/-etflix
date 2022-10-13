463f495f1a0f2ee2cb51c3b8ed33187f60c34b81
Guillermina <68613395+Guillerminav@users.noreply.github.com>
2022-10-13 15:03:29 -0300

    Filtro por categorias

--- index.js
+++ index.js
@@ -50,39 +50,39 @@ const info = [
         keywords: ['comedia', 'romantica']
     },
     {
-      img: './assets/img9.webp',
-      title: 'El Ilusionista',
-      description: 'Con los ojos puestos en una hermosa aristócrata, un talentoso ilusionsita llamado Eisenheim usa sus poderes para ganársela a su prometido: un príncipe heredero.',
-      stars: 4,
-      keywords: ['comedia', 'romantica']
+        img: './assets/img9.webp',
+        title: 'El Ilusionista',
+        description: 'Con los ojos puestos en una hermosa aristócrata, un talentoso ilusionsita llamado Eisenheim usa sus poderes para ganársela a su prometido: un príncipe heredero.',
+        stars: 4,
+        keywords: ['comedia', 'romantica']
     },
     {
-      img: './assets/img10.webp',
-      title: 'Un lugar llamado Notthing Hill',
-      description: 'Un encuentro casual une al reservado dueño de una librería William Thacker y al ícono de Hollywood Anna Scott, quienes se entregan a un romance improbable.',
-      stars: 4.5,
-      keywords: ['comedia', 'romantica']
+        img: './assets/img10.webp',
+        title: 'Un lugar llamado Notthing Hill',
+        description: 'Un encuentro casual une al reservado dueño de una librería William Thacker y al ícono de Hollywood Anna Scott, quienes se entregan a un romance improbable.',
+        stars: 4.5,
+        keywords: ['comedia', 'romantica']
     },
     {
-      img: './assets/img11.jpg',
-      title: 'Mis dos vidas',
-      description: 'En su graduación universitaria, la vida de Natalie se divide en dos realidades paralelas tras realizar una prueba de embarazo. ¿Qué le deparará el futuro?.',
-      stars: 3,
-      keywords: ['comedia', 'romantica']
+        img: './assets/img11.jpg',
+        title: 'Mis dos vidas',
+        description: 'En su graduación universitaria, la vida de Natalie se divide en dos realidades paralelas tras realizar una prueba de embarazo. ¿Qué le deparará el futuro?.',
+        stars: 3,
+        keywords: ['comedia', 'romantica']
     },
     {
-      img: './assets/img12.webp',
-      title: 'Ni en sueños',
-      description: 'Un inesperado reencuentro lleva a una obstinada candidata presidencial a contratar a un imprudente redactor de discursos que desafía sus políticas... y su corazón.',
+        img: './assets/img12.webp',
+        title: 'Ni en sueños',
+        description: 'Un inesperado reencuentro lleva a una obstinada candidata presidencial a contratar a un imprudente redactor de discursos que desafía sus políticas... y su corazón.',
       stars: 2.6,
       keywords: ['comedia', 'romantica']
     },
     {
-      img: './assets/img13.webp',
-      title: 'Orgullo y prejuicio',
-      description: 'En la novela de Jane Austen del siglo XIX, la Sra. Bennet quiere casar a sus hijas con prósperos caballeros, incluido el recién llegado Sr. Darcy.',
-      stars: 4,
-      keywords: ['drama', 'romantica']
+        img: './assets/img13.webp',
+        title: 'Orgullo y prejuicio',
+        description: 'En la novela de Jane Austen del siglo XIX, la Sra. Bennet quiere casar a sus hijas con prósperos caballeros, incluido el recién llegado Sr. Darcy.',
+        stars: 4,
+        keywords: ['drama', 'romantica']
     },
 ]
 
@@ -91,16 +91,14 @@ const info = [
 
 let container = document.querySelector('.container');
 
-const addMovie = (img, title, description, stars) => {
+const addMovie = (img, title, description, stars, keywords) => {
 
     // Creacion de elementos
     let movieContainer = document.createElement('DIV')
     let movieImage = document.createElement('IMG')
     let movieTitle = document.createElement('H2')
     let movieDescription = document.createElement('P')
-    // let movieStars = document.createElement('P')
-    // let watchNow = document.createElement('BUTTON')
-    // let buttonContainer = document.createElement('DIV')
+    let movieKeywords = document.createElement('P')
 
     // Agregamos clases
     movieImage.setAttribute("src", img)
@@ -108,26 +106,21 @@ const addMovie = (img, title, description, stars) => {
     movieImage.classList.add('movieImage')
     movieTitle.classList.add('movieTitle')
     movieDescription.classList.add('movieDescription')
-    // movieStars.classList.add('movieStars')
-    // watchNow.classList.add('watchButton')
-    // buttonContainer.classList.add('buttonContainer')
+    movieKeywords.classList.add('keywords')
 
     // Agregamos textNodes
     movieTitle.textContent = title
     movieDescription.textContent = description
-    //movieStars.textContent = stars
-    //watchNow.textContent = 'Ver ahora'
+    movieKeywords.textContent = keywords
 
-
-    //buttonContainer.appendChild(movieStars)
-    //buttonContainer.appendChild(watchNow)
     movieContainer.appendChild(movieImage)
     movieContainer.appendChild(movieTitle)
     movieContainer.appendChild(movieDescription)
+    movieContainer.appendChild(movieKeywords)
     movieTitle.style.display = 'none'
     movieDescription.style.display = 'none'
+    movieKeywords.style.display = 'none'
 
-    //movieContainer.appendChild(buttonContainer)
 
     // Eventos
     movieContainer.addEventListener('mouseover', () => {
@@ -152,7 +145,7 @@ const addMovie = (img, title, description, stars) => {
       }
 
     // Ventana modal
-    movieContainer.addEventListener('click', (e) => {
+    movieContainer.addEventListener('click', () => {
         let modalImg = document.querySelector('.modalImage')
         let modalTitle = document.querySelector('.modalTitle')
         let modalDescription = document.querySelector('.modalDescription')
@@ -170,7 +163,7 @@ const addMovie = (img, title, description, stars) => {
 let elementoTemporal = document.createDocumentFragment();
 
 for (let i = 0; i < info.length; i++) {
-    let newMovie = addMovie(info[i].img, info[i].title, info[i].description, info[i].stars);
+    let newMovie = addMovie(info[i].img, info[i].title, info[i].description, info[i].stars, info[i].keywords);
     elementoTemporal.appendChild(newMovie);
 }
 
@@ -199,6 +192,45 @@ inputSearch.addEventListener('keyup', (e) => {
         }
     }
 })
+
+// Filtors por categorias
+
+const dropdownItem = document.querySelectorAll('.dropdown-item')
+
+
+dropdownItem.forEach(item => {
+    item.addEventListener('click', (e) => {
+        let clicked = e.target.innerText.toLowerCase()
+        let er = new RegExp(clicked, "i")
+        for (let i = 0; i < items.length; i++){ 
+            let valor = items[i]
+            if(er.test(valor.innerText)){
+                console.log('INCLUIDO')
+                valor.classList.remove("filter")
+            }else{
+                valor.classList.add("filter")
+            }
+        }
+    })
+})
+
+
+// Dropdown items
+
+const listItem = document.querySelectorAll('.toggle')
+const dropdownItems = document.querySelectorAll('.dropdown-menu')
+
+const showToggle = () => {
+    dropdownItems.forEach(dropdown => {
+        dropdown.classList.toggle('show')
+    })
+}
+
+listItem.forEach(item => {
+    item.addEventListener('click', showToggle)
+})
+
+
 
 // Falta resolver:
 // - centrar el contenido y que al wrappearse no queden los items centrados
